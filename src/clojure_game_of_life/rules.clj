32c1ce7ac743e09b78efdb51b1a0b423c17e0de1
Simon Jones <simonp.jones@ntlworld.com>
2020-11-27 20:41:53 +0000

    Implementing game of life's first two rules

--- src/clojure_game_of_life/rules.clj
+++ src/clojure_game_of_life/rules.clj
@@ -4,3 +4,63 @@
     "Generates a new game of life world from the given size"
     [size]
     (map #(repeat size %) (repeat size 0)))
+
+(defn get-cell-value
+    [x y world]
+    (if (and (>= x 0) (>= y 0) (< x (count (first world))) (< y (count world)))
+        (nth (nth world y) x)))
+
+(defn upper-left
+    [x y world]
+    (get-cell-value (dec x) (dec y) world))
+
+(defn upper
+    [x y world]
+    (get-cell-value x (dec y) world))
+
+(defn upper-right
+    [x y world]
+    (get-cell-value (inc x) (dec y) world))
+
+(defn left
+    [x y world]
+    (get-cell-value (dec x) y world))
+
+(defn right
+    [x y world]
+    (get-cell-value (inc x) y world))
+
+(defn lower-left
+    [x y world]
+    (get-cell-value (dec x) (inc y) world))
+
+(defn lower
+    [x y world]
+    (get-cell-value x (inc y) world))
+
+(defn lower-right
+    [x y world]
+    (get-cell-value (inc x) (inc y) world))
+
+(def neighbours [upper-left upper upper-right left right lower-left lower lower-right])
+
+(defn neighbour-count
+    [x y world]
+    (count (filter #(= (% x y world) 1) neighbours)))
+
+(defn enforce-rule-two
+    "Any live cell with two or three live neighbours lives on to the next generation"
+    [x y world]
+    (if (= (neighbour-count x y world) 2)
+        (get-cell-value x y world)
+        0))
+
+(defn next-iteration
+    "Calculates the next iteration of game of life from the previous iteration"
+    [world]
+    (map-indexed 
+        (fn [row-index row] 
+            (map-indexed 
+                (fn [col-index cell] (enforce-rule-two col-index row-index world)) 
+                row))
+        world))
--- test/clojure_game_of_life/tests.clj
+++ test/clojure_game_of_life/tests.clj
@@ -2,6 +2,8 @@
     (:use clojure.test)
     (:use clojure-game-of-life.rules))
 
+(def dead-world [[0 0 0] [0 0 0] [0 0 0]])
+
 (deftest world-gen
     (testing "Creates a 1x1 world" 
         (let [world (generate-world 1)]
@@ -12,3 +14,25 @@
         (let [world (generate-world 5)]
             (is (= (count world) 5))
             (is (= (count (first world)) 5)))))
+
+(deftest rule-one
+    "Any live cell with fewer than two live neighbours dies, as if by underpopulation"
+    
+    (testing "No neighbours"
+        (let [world [[0 0 0] [0 1 0] [0 0 0]]]
+            (is (= (next-iteration world) dead-world))))
+            
+    (testing "One neighbour"
+        (let [world [[0 0 0] [0 1 1] [0 0 0]]]
+            (is (= (next-iteration world) dead-world))))
+            
+    (testing "Corner cell"
+        (let [world [[1 0 0] [0 0 0] [0 0 0]]]
+            (is (= (next-iteration world) dead-world)))))
+
+(deftest rule-two
+    "Any live cell with two or three live neighbours lives on to the next generation"
+    
+    (testing "Two neighbours"
+        (let [world [[0 0 0] [1 1 1] [0 0 0]]]
+            (is (= (next-iteration world) [[0 0 0] [0 1 0] [0 0 0]])))))
